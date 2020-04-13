69f1493df9e7dc4fdce0cc1a06216a6b1aee3baf
HariniJeyaraman <58400069+HariniJeyaraman@users.noreply.github.com>
2020-04-14 00:54:42 +0530

    updated code for cases when array isn't rotated
    Ex : a={1, 7, 67, 133, 178}

--- search_element_sorted_rotated_array.cpp
+++ search_element_sorted_rotated_array.cpp
@@ -13,33 +13,51 @@ int find_pivot(std::vector<int> a, int beg, int end)
         else if(a[mid]>a[end])
             beg=mid+1;
         else
-            end=mid;
-    }
-}
-
-int search_element(std::vector<int> a, int key, int beg, int end)
-{
-    int pivot = find_pivot(a,0,a.size()-1);
-    if(key>=a[pivot] && key<=a[end])
-        beg=pivot;
-    else
-        end=pivot;
-    while(beg<=end)
-    {
-        int mid=(beg+end)/2;
-        if(key==a[mid])
-            return mid;
-        else if(key>a[mid])
-            beg=mid+1;
-        else
             end=mid-1;
     }
     return -1;
 }
 
+
+int search_element(std::vector<int> a, int key, int beg, int end)
+{
+    int pivot = find_pivot(a,0,a.size()-1);
+    //If input is rotated
+    if(pivot!=-1)
+    {
+        if(key>=a[pivot] && key<=a[end])
+        beg=pivot;
+        else
+            end=pivot;
+        while(beg<=end)
+        {
+            int mid=(beg+end)/2;
+            if(key==a[mid])
+                return mid;
+            else if(key>a[mid])
+                beg=mid+1;
+            else
+                end=mid-1;
+        }
+    }
+    while(beg<=end)
+        {
+            int mid=(beg+end)/2;
+            if(key==a[mid])
+                return mid;
+            else if(key>a[mid])
+                beg=mid+1;
+            else
+                end=mid-1;
+        }
+
+    
+    return -1;
+}
+
 int main()
 {
-    std::vector<int> a={14,15,16,19,23,6,7,8,9};
-    std::cout<<"The index of element is : "<<search_element(a,7,0,a.size()-1)<<"\n";
+    std::vector<int> a={1, 7, 67, 133, 178};
+    std::cout<<"The index of element is : "<<search_element(a,1,0,a.size()-1)<<"\n";
     return 0;
 }
