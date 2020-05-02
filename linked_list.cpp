aca7220df4f545ae2aa4d614354a352609da2e3d
HariniJeyaraman <58400069+HariniJeyaraman@users.noreply.github.com>
2020-05-02 17:59:54 +0530

    Add files via upload

---
+++ linked_list.cpp
@@ -0,0 +1,77 @@
+//Implement Linked List
+#include<iostream>
+#include<cstdlib>
+#include<vector>
+using namespace std;
+
+struct node
+{
+    int data;
+    struct node* nptr;
+};
+
+struct node* hptr=NULL;
+
+void insertNode(int pos, int x)
+{
+    struct node *temp=new node;
+    if(temp==NULL)
+        cout<<"Insert not possible\n";
+    temp->data=x;
+    if(pos==1)
+    {
+        temp->nptr=hptr;
+        hptr=temp;
+    }
+    else
+    {
+        int i=1;
+        struct node* thptr=hptr;
+        while(i<pos-1)
+        {
+            thptr=thptr->nptr;
+            i++;
+        }
+        temp->nptr=thptr->nptr;
+        thptr->nptr=temp;
+    }
+}
+
+void deleteNode(int pos)
+{
+    struct node *temp=hptr;
+    int i=1;
+    if(temp==NULL)
+        cout<<"Delete not possible\n";
+    else
+    {
+        while(i<pos-1)
+        {
+            temp=temp->nptr;
+            i++;
+        }
+        temp->nptr=(temp->nptr)->nptr;
+    }
+}
+
+void print()
+{
+    struct node* temp=hptr;
+    while(temp!=NULL)
+    {
+        cout<<temp->data<<"\n";
+        temp=temp->nptr;
+    }
+}
+
+int main()
+{
+    insertNode(1,10);
+    insertNode(2,20);
+    insertNode(3,30);
+    insertNode(4,40);
+    insertNode(5,50);
+    deleteNode(3);
+    print();
+    return 0;
+}
