0b2f56e8d19bda18d47682f3fab706b66c7cad93
HariniJeyaraman <58400069+HariniJeyaraman@users.noreply.github.com>
2020-07-24 12:15:06 +0530

    added 2nd method to Flatten Binary Tree to Linked List

--- flatten_binary_tree_to_ll.cpp
+++ flatten_binary_tree_to_ll.cpp
@@ -39,6 +39,7 @@ The flattened tree should look like:
  * };
  */
 
+//Method 1
 //Store all the nodes in a vector in preorder and assign the vector values to the right of each node in tree
 //and assign left as NULL 
 //in place conversion from a tree to a linked list
@@ -64,3 +65,34 @@ public:
         result[result.size()-1]->left= result[result.size()-1]->left = NULL;
     }
 };
+
+//Method 2
+/*For a subtree, we store the root's right child in a temporary variable and store root's left child in root->right
+ Then we traverse to the rightmost child of the root and make that child's right as the temporary variable.
+ Consider this subtree: 
+   1
+  / \
+ 2   3
+
+We store node 3 in a temporary variable. Then we make node 2 as the right child of root node, 1
+Then we go to the rightmost node from root, that is 2, and make the right node of 2 as the node in the temp variable, ie 3
+*/
+
+TreeNode* getRightMost(TreeNode* root){
+        // if(root==NULL) return root;
+        if(root->right==NULL) return root;
+        return getRightMost(root->right);
+    }
+    
+    void flatten(TreeNode* root) {
+        if(root==NULL)
+            return;
+        flatten(root->left);
+        flatten(root->right);
+        TreeNode* temp = root->right;
+        root->right = root->left;
+        root->left=NULL;
+        TreeNode* rightnode = getRightMost(root);
+        rightnode->right = temp;
+    }
+ 
